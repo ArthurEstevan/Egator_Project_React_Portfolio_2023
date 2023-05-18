687fafe4c70f2fdf53757634d5726e7eb1d2f762
Arthur <arthur.estevan01@gmail.com>
2023-05-18 15:16:42 -0300

    Update Footer

--- react-portfolio/src/components/footer/Footer.jsx
+++ react-portfolio/src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a className='footer__logo' href="#">Arthur Estevan Vargas</a>
+      
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#abouts">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonial">Testimonial</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+    <div className="footer__socials">
+      <a href="https://facebook.com"><FaFacebookF /></a>
+      <a href="https://instagram.com"><FiInstagram /></a>
+      <a href="https://twitter.com"><IoLogoTwitter /></a>
+    </div>
+
+    <div className="footer__copyright">
+      <small>&copy; Arthur Estevan Vargas. All rights reserved.</small>
+    </div>
+    </footer>
   )
 }
 
